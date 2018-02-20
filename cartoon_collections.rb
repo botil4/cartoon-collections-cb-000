5953988e73b2748b105fe91dde35515ebfd75062
Travis Clarkson <tk1667@gmail.com>
2018-02-20 01:17:33 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map do |call|
-    array = call.split
+    array = call.split('')
     binding.pry
     array.first.upcase
     "#{array.join}!"
